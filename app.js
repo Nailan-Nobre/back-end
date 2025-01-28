df73be798448efc25eb6869cde54e6fed27f68a1
Nailan Nobre <nailannobre22@gmail.com>
2025-01-28 18:16:19 -0300

    criptografia

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-import cors from 'cors'
-import Express from 'express'
-import logger from './midlewares/logger.js'
-import userRouter from './router/users.js'
+import cors from 'cors';
+import Express from 'express';
+import logger from './midlewares/logger.js';
+import userRouter from './router/users.js';
+import verifyToken from './middlewares/verifyToken.js';
+import authRouter from './router/auth.js';
 
 const app = Express()
 app.use(cors())
 app.use(Express.json())
 
-app.use(logger)
-
 app.get('/', (req, res) => {
   res.send("App online!")
 })
 
+app.use(logger)
+app.use(authRouter)
+
+app.use(verifyToken)
 app.use(userRouter)
 
 const PORT = process.env.PORT || 3000
