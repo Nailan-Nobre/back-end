b1e3ea2aee49d7e80fb5d819a8b1fd29e1c2998e
Nailan Nobre <nailannobre22@gmail.com>
2025-02-12 20:47:29 -0300

    ...

--- midlewares/upload.js
+++ midlewares/upload.js
@@ -6,7 +6,7 @@ const storage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: 'back-end',
-    allowedFormats: ['jpg', 'png', 'jpeg'],
+    format: async (req, file) => 'png',
     public_id: (req, file) => file.originalname,
   },
 });
--- router/cloudinary.js
+++ router/cloudinary.js
@@ -3,10 +3,19 @@ const upload = require('../midlewares/upload');
 const prisma = require('../prisma');
 const router = express.Router();
 
+import upload from '../midlewares/upload.js';
+
 router.post('/upload', upload.single('foto'), async (req, res) => {
     try {
+        console.log("Arquivo recebido:", req.file);
+        console.log("Dados recebidos:", req.body);
+
+        if (!req.file) {
+            return res.status(400).json({ error: "Nenhuma imagem foi enviada" });
+        }
+
         const { email, name, password, telefone, estado, cidade, tipo } = req.body;
-        const foto = req.file.path;
+        const foto = req.file.path; // URL da imagem no Cloudinary
 
         const novoUsuario = await prisma.user.create({
             data: {
@@ -17,14 +26,13 @@ router.post('/upload', upload.single('foto'), async (req, res) => {
                 foto,
                 estado,
                 cidade,
-                tipo: tipo.toUpperCase() // Certifique-se de que o tipo está em maiúsculas para corresponder ao enum
+                tipo: tipo.toUpperCase()
             },
         });
 
         res.json(novoUsuario);
     } catch (error) {
+        console.error('Erro ao salvar a imagem:', error);
         res.status(500).json({ error: "Erro ao salvar a imagem" });
     }
 });
-
-module.exports = router;
