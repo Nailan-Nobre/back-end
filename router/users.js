5d15839a8fb289d0f0390ce2e82074696cf219e8
Nailan Nobre <161063663+Nailan-Nobre@users.noreply.github.com>
2025-02-24 20:55:27 -0300

    Update users.js

--- router/users.js
+++ router/users.js
@@ -4,27 +4,17 @@ import { Router } from 'express';
 const userRouter = Router();
 const prisma = new PrismaClient();
 
-// Rota para buscar manicures
-userRouter.get('/users/manicures', async (req, res) => {
+// Rota para buscar todas as manicures
+userRouter.get('/manicures', async (req, res) => {
   try {
     const manicures = await prisma.user.findMany({
-      where: { tipo: "MANICURE" },
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        telefone: true,
-        estado: true,
-        cidade: true,
-        tipo: true,
-      }
+      where: { tipo: "MANICURE" }
     });
 
     res.json(manicures);
   } catch (error) {
-    console.error("Erro ao buscar manicures:", error);
     res.status(500).json({ error: 'Erro ao buscar manicures' });
   }
 });
 
-export default userRouter;
+export default userRouter
