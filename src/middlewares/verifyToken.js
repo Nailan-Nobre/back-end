b2e07ca0c7e7b2680ae373776c67dbe20f30dc59
Nailan-Nobre <nailannobre22@gmail.com>
2025-06-09 21:49:22 -0300

    agendamento

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,24 +1,26 @@
 import jwt from 'jsonwebtoken';
 
 function verifyToken(req, res, next) {
-  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.split(' ')[1];
 
   if (!token) {
-    return res.status(403).send({
+    return res.status(403).json({
       auth: false,
-      message: 'Nenhum token fornecido.',
+      message: 'Token ausente ou mal formatado.',
     });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(500).send({
+      return res.status(401).json({
         auth: false,
-        message: 'Falha ao autenticar. Error -> ' + err,
+        message: 'Token inválido: ' + err.message,
       });
     }
 
-    req.user = decoded; // Agora o user tem id, tipo, etc.
+    // ⚠️ Corrigido aqui
+    req.user = decoded; // Isso inclui id, tipo e outros dados do token
     next();
   });
 }
