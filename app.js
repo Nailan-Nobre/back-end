9174ae6e4a293d50442ddf1951b9318811d648f4
Nailan Nobre <nailannobre22@gmail.com>
2025-06-14 11:43:32 -0300

    agendamento7

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = Express();
 const corsOptions = {
   origin: [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://ifpi-picos.github.io'
   ],
   credentials: true
 };
